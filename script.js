1fbf454fc7b0082d1d753b0f82a2cc2895022f2b
VaderSteve76 <alverson.s@yahoo.com>
2020-05-18 23:58:02 -0400

    Added event listeners

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ const result = document.getElementById('result');
 const more = document.getElementById('more');
 
 const apiURL = 'https://api.lyrics.ovh';
+
+
+// Event listeners
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert('Please type in a search term');
+  } else {
+    searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics button click
+result.addEventListener('click', e => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
+  }
+});
